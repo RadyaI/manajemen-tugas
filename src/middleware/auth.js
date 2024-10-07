0669dd2b3c822eedeeb759c48055c5227a244e3d
RadyaI <radyaiftikhar@gmail.com>
2024-10-07 10:00:25 +0700

    tambah atmin

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,5 +1,5 @@
 function isAdmin(email) {
-    const adminList = ['radyaiftikhar@gmail.com','radyaproject@gmail.com']
+    const adminList = ['radyaiftikhar@gmail.com','radyaproject@gmail.com', 'bagusramanda81@gmail.com']
     return adminList.includes(email)
 }
 
